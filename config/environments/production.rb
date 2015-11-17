9ed2c5316343dc40fb55362b5d14d618d5b138f1
Kelvin <kelvin_guerrero@hotmail.com>
2015-11-17 14:24:21 -0500

    Cambios bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,8 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  #config.consider_all_requests_local       = false CAMBIO PARA VER ERRORES
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -42,7 +43,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  #config.log_level = :info
+  #config.log_level = :info CAMBIO PARA VER ERRORES
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
